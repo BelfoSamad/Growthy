dbffa0fa97901cdb88974126469b159e866058ea
BelfoSamad <a.belfodil@esi-sba.dz>
2020-06-04 17:34:53 +0100

    abdessamed's prototype - update #2

--- Prototypes/abdessamed/background.js
+++ Prototypes/abdessamed/background.js
@@ -1,7 +1,35 @@
+var Timer = function(callback, delay) {
+  var timerId, start, remaining = delay;
+  
+  this.pause = function() {
+    window.clearTimeout(timerId);
+    remaining -= Date.now() - start;
+    console.log(Date.now());
+  };
+  
+  this.resume = function() {
+    start = Date.now();
+    window.clearTimeout(timerId);
+    if(remaining > 0)
+      timerId = window.setTimeout(callback, remaining);
+  };
+  
+  this.resume();
+};
+
+var timer = new Timer(function() {
+  //chrome.tabs.update(null, { url: "https://www.facebook.com" });
+}, 60000);
+
+//-----------------------------------------------------
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (request.test == "hello"){
-      chrome.tabs.update({ url: "htts://www.facebook.com" });
-      sendResponse({farewell: "goodbye"});
+    if (request.state == "Paused"){
+      console.log("Pausing Timer");
+      timer.pause();
+    } else if (request.state == "Resumed"){
+      console.log("Resuming Timer");
+      timer.resume();
     }
+    sendResponse({farewell: "goodbye"});
  });
--- Prototypes/abdessamed/content.js
+++ Prototypes/abdessamed/content.js
@@ -1,7 +1,18 @@
-let interval = setInterval(function(){
-	alert("Hello");
-	clearInterval(interval);
-	chrome.runtime.sendMessage({test: "hello"}, function(response) {
-		console.log(response.farewell);
-	});
-}, 3000);
+alert("In");
+
+let video = document.getElementsByTagName("video")[0];
+let video_url = document.getElementsByTagName("ytd-watch-flexy")[0].videoId;
+
+if (video != null) {
+	video.onpause = function () {
+		chrome.runtime.sendMessage({state: "Paused", src: video_url, time: video.currentTime},function(response){
+			console.log("Paused Timer");
+		});
+	};
+
+	video.onplaying = function () {
+		chrome.runtime.sendMessage({state: "Resumed", src: video_url, time: video.currentTime},function(response){
+			console.log("Resumed Timer");
+		});
+	};
+}
