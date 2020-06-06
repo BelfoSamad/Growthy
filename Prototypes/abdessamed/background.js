79d4651d52adb834999e3cf354279ebf813a60e6
BelfoSamad <a.belfodil@esi-sba.dz>
2020-06-06 14:54:18 +0100

    Prototype final

--- Prototypes/abdessamed/background.js
+++ Prototypes/abdessamed/background.js
@@ -1,24 +1,68 @@
-//Custom Timer to be able to pause/resume it
-var Timer = function (callback, delay) {
-  var timerId, start, remaining = delay;
+//Declaration
+let watch_time = 3;//3 minutes
+//Timer
+var CustomTimer = function (callback){
+  let timerId;
 
-  this.pause = function () {
-    window.clearTimeout(timerId);
-    remaining -= Date.now() - start;
-  };
+  this.start = (new_remaining) => {
+    remaining = new_remaining * 60;
+    let minutes = Math.floor(remaining / 60);
+    let seconds = remaining - minutes * 60;
+    chrome.browserAction.setBadgeText({text: minutes + ":" + seconds});
+  }
 
-  this.resume = function () {
-    start = Date.now();
-    window.clearTimeout(timerId);
-    if (remaining > 0)
-      timerId = window.setTimeout(callback, remaining);
-  };
+  this.resume = () => {
+    timerId = setInterval(() => {
+      remaining = remaining - 1;
+      if (remaining > 0) {
+        let minutes = Math.floor(remaining / 60);
+        let seconds = remaining - minutes * 60;
+        chrome.browserAction.setBadgeText({text: minutes + ":" + seconds});
+      }else {
+        callback();
+      }
+    }, 1000);
+  }
+
+  this.pause = () => {
+    clearInterval(timerId);
+  }
 
   this.resume();
-};
+}
 
-//inject the content script on new youtube video tabs (because youtube works with ajax so the tab actually doesn't change so the content
-//script can't be updated)
+/* -------------------------------- Firebase Setup ---------------------------------
+var firebaseConfig = {
+  apiKey: "AIzaSyD8lAoB_uRtirEjew0CnKTH_VNhWA0zhJ0",
+  authDomain: "extension-e927c.firebaseapp.com",
+  databaseURL: "https://extension-e927c.firebaseio.com",
+  projectId: "extension-e927c",
+  storageBucket: "extension-e927c.appspot.com",
+  messagingSenderId: "1080995667086",
+  appId: "1:1080995667086:web:0d42767c0cd92367573ddf",
+  measurementId: "G-EQ0X3P6SKC"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//Storage
+var storage = firebase.storage();
+var storageRef = storage.ref();
+
+storageRef.child('games/game1/tab.html').getDownloadURL().then(function(url) {
+  var xhr = new XMLHttpRequest();
+  xhr.responseType = 'blob';
+  xhr.onload = function(event) {
+    var blob = xhr.response;
+  };
+  xhr.open('GET', url);
+  xhr.send();
+}).catch(function(error) {
+  // Handle any errors
+});*/
+/*------------------------ Injecting Script to Videos Tabs -------------------------------------
+  inject the content script on new youtube video tabs (because youtube works with ajax so the tab
+  actually doesn't change so the content script can't be updated)*/
 tabs_with_scripts = [];
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status == "loading") {
@@ -40,22 +84,28 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   }
 });
 
-//start the timer when chrome is launched
-var timer = new Timer(function () {
+/*------------------------ Start Timer and getting states form conetnt scripts --------------------*/
+var timer = new CustomTimer(function () {
   //when the timer ends, go through tabs and get the activated one (that's just to try we're supposed to get the tabs with youtube)
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     //send a message to the content script to pause the video (that enables us to get the url/time of the video)
     chrome.tabs.sendMessage(tabs[0].id, { action: "Pause" }, function (response) {
       console.log("Pause Video");
-      //update the content of the tab to our html file (we can include js and css on it so we good)
-      chrome.tabs.update(null, { url: chrome.extension.getURL("tab.html?next=" + response.url + "&t=" + response.time) });
+
+      //update the Time and pause it (for the next run)
+      timer.start(watch_time);
+      timer.pause();
+      
+      //Remove the tab we're in (to avoid goinf back to youtube and creating a new tab)
+      chrome.tabs.remove(tabs[0].id);
+      chrome.tabs.create({url: chrome.extension.getURL("game/tab.html?next=" + response.url.split("&")[0]
+        + "&t=" + response.time)});
     });
   });
-
-}, 10000);
-//pause it and wait when a video started (to start counting)
+});
+//Start the Timer First Time
+timer.start(watch_time);
 timer.pause();
-
 //-----------------------------------------------------
 //get states from the content script
 chrome.runtime.onMessage.addListener(
