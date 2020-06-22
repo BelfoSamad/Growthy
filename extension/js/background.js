552dbb9e2238f6fadd068fc2652ac3b751cbceac
BelfoSamad <a.belfodil@esi-sba.dz>
2020-06-22 11:57:34 +0100

    Added Disable/Enable Extension Feature

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,199 +1,51 @@
-//Getting data from popup (through session)
-//let watch_time = sessionStorage.getItem("watch-time");
-//let game_time = sessionStorage.getItem("game-time");
-//let enabled = sessionStorage.getItem("enabled"); (use chrome.runtime.reload() to update
-//background script on runtime)
-
+/*---------------------------------- Declarations -------------------------------------------*/
 //Declarations (Temporary)
 let watch_time = 0.5;
 let game_time = 0.3;
+
+/*--------------------------------- Extension State -----------------------------------------*/
+//Check if Extension is Enabled or Disabled
 let enabled = true;
-
-if (enabled) {
-  /*-------------------------------------- Timer -------------------------------------------------*/
-  var Timer = function (callback) {
-    let timerId;
-    let remaining = 0;
-    let paused = true;
-
-    this.start = (delay) => {
-      console.log("Timer Set");
-      remaining = delay * 60;
-      let minutes = Math.floor(remaining / 60);
-      let seconds = remaining - minutes * 60;
-      chrome.browserAction.setBadgeText({ text: minutes + ":" + seconds });
-      chrome.browserAction.setBadgeBackgroundColor({ color: "#e67e22" });
-    }
-
-    this.pause = () => {
-      if (!paused) {
-        console.log("Timer Paused");
-        if (timerId != null) clearInterval(timerId);
-        paused = true;
-      }
-    }
-
-    this.resume = () => {
-      if (paused) {
-        console.log("Timer Resumed");
-        paused = false;
-        timerId = setInterval(() => {
-          remaining = remaining - 1;
-          if (remaining > 0) {
-            let minutes = Math.floor(remaining / 60);
-            let seconds = remaining - minutes * 60;
-            chrome.browserAction.setBadgeText({ text: minutes + ":" + seconds });
-            chrome.browserAction.setBadgeBackgroundColor({ color: "#e67e22" });
-          } else {
-            callback();
-          }
-        }, 1000);
-      }
-    }
-  }
-
-  /*------------------------------ Inject Content Scripts ----------------------------------------*/
-  let tabs_with_scripts = [];
-
-  //When a tab is updated or created
-  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status == "loading")
-      if (tab.url.startsWith("https://www.youtube.com/watch?v=")) {
-        //Inject content script
-        chrome.tabs.executeScript(tabId, { file: "js/content.js" }, () => {
-          console.log("YW Tab: Injecting");
-          if (!tabs_with_scripts.includes(tabId)) {
-            console.log("Adding Tab Id");
-            tabs_with_scripts.push(tabId);
-          }
-
-          //Resume Timer
-          timer.resume();
-        });
-      } else {
-        console.log("NYW: Nothing");
-        if (tabs_with_scripts.includes(tabId)) {
-          console.log("NYW: Tab Id is in the array -> Remove id");
-          tabs_with_scripts.pop(tabId);
-          timer.pause();
-        }
-      }
-  });
-
-  //Remove tabId from tabs_with_scripts when tab is removed
-  chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
-    if (tabs_with_scripts.includes(tabId)) {
-      console.log("Tab Id was in the array now it's removed -> Remove id");
-      tabs_with_scripts.pop(tabId);
-
-      //Pause Timer
-      timer.pause();
-    }
-  });
-
-  /*---------------------------------- Timer Management ------------------------------------------*/
-  //Setup Timer
-  var timer = new Timer(function () {
-    //Callback after timer finishes
-    //Get the Id of the activated tab
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      //send a message to the content script to pause the video (that enables us to get the url/time of the video)
-      chrome.tabs.sendMessage(tabs[0].id, { action: "Pause" }, function (response) {
-
-        //update the Timer
-        console.log("Update Timer");
-        timer.start(watch_time);
-
-        //For Testing
-        //alert(data.url.split("&")[0] + "&t=" + data.time);
-        injectGame(tabs[0].id, response);
-      });
-    });
-  });
-
-  //Start Timer (First run)
-  timer.start(watch_time);
-
-  //Getting Requests from content scripts
-  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-
-    switch (request.state) {
-      case "Paused":
-        console.log("Paused State Arrived");
-        timer.pause();//When Video Paused (or some type of error)
-        break;
-      case "Resumed":
-        console.log("Resumed State Arrived");
-        timer.resume();//When Video started or resumed after it being paused
-        break;
-    }
-
-    sendResponse("Alright");
-  });
-
-  //Getting state of the new activated tab
-  chrome.tabs.onActivated.addListener((activeInfo) => {
-    if (tabs_with_scripts.includes(activeInfo.tabId)) {
-      console.log("The Tab You switched to is YW");
-      chrome.tabs.sendMessage(activeInfo.tabId, { action: "State" }, (response) => {
-        if (response.paused == true) {
-          console.log("State of current tab : Paused");
-          timer.pause();
+chrome.storage.sync.get(["enabled"], (items) => {
+  enabled = items[0];
+});
+//Listen if Extension State Changed
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+  for (var key in changes) {
+    var storageChange = changes[key];
+    switch (key) {
+      case "enabled":
+        enabled = storageChange.newValue;
+        console.log(enabled);
+        if (enabled) {
+          startProcess();
         } else {
-          console.log("State of current tab : Resumed");
-          timer.resume();
+          //Stop and Unset Time Object
+          if (timer != null) {
+            timer.pause();
+            timer = null;
+          }
+          //Remove All Listeners
+          chrome.runtime.onMessage.removeListener(gamesRequests);
+          chrome.tabs.onActivated.removeListener(onTabActivated);
+          chrome.runtime.onMessage.removeListener(contentScriptsRequests);
+          chrome.tabs.onRemoved.removeListener(onTabRemoved);
+          chrome.tabs.onUpdated.removeListener(onTabUpdated);
+
+          //Change Badge
+          chrome.browserAction.setBadgeText({ text: "Off" });
+          chrome.browserAction.setBadgeBackgroundColor({ color: "#bdc3c7" });
         }
-      });
-    } else {
-      console.log("The Tab You switched to is NYW");
-      timer.pause();
-    }
-  });
-
-  /*---------------------------------- Injecting Games -------------------------------------------*/
-  injectGame = (tabId, data) => {
-
-    //Remove the tab we're in (to avoid going back to youtube and creating a new tab)
-    chrome.tabs.remove(tabId);
-
-    //Create New Tab
-    chrome.tabs.create({ url: chrome.runtime.getURL("../games/game_test/index.html") }, (tab) => {
-
-      //Don't Know why this works
-      chrome.tabs.executeScript({ file: "js/game_manager.js" }, () => {
-        console.log("Content Script Injected");
-
-        //Send Youtube/Game Data to Game Tab (TODO: Change the structure of Game Data)
-        chrome.tabs.sendMessage(tab.id, { action: "Next", url: data.url.split("&")[0], time: data.time, level: 1 }, (response) => {
-          console.log(response.msg);
-        });
-
-        //Send an Action call to the tab to go back
-        window.setTimeout(() => {
-          chrome.tabs.sendMessage(tab.id, { action: "Back" }, (response) => {
-
-            //TODO: the response will have the current game level to save it
-
-            //Remove the current tab (with the game to avoid reusing it)
-            chrome.tabs.remove(tab.id);
-
-            //Go back to youtube (where we were at)
-            chrome.tabs.create({ url: response.url });
-          });
-        }, game_time * 60 * 1000);
-      });
-    });
-  }
-
-  //Receive Data/Actions From Game
-  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-
-    switch (request.action) {
-      case "New":
-        //Start Getting New Game
         break;
     }
+  }
+});
 
-    sendResponse("Alright");
-  });
+/*--------------------------------- Start Listening -------------------------------------------*/
+if (enabled) {
+  startProcess();
+} else {
+  chrome.browserAction.setBadgeText({ text: "Off" });
+  chrome.browserAction.setBadgeBackgroundColor({ color: "#bdc3c7" });
 }
+
--- extension/js/popup.js
+++ extension/js/popup.js
@@ -56,15 +56,18 @@ $(function () {
 
     //--------------------------------------------------
     // enable/disable extension
-    /*--------------------------------------------------
+    //--------------------------------------------------
 
-    let switchButtpn = "off";
+    let switchButton = "on";
+    $("#extension_state_switch").prop("checked", true);
     //get the saved state of the extension from storage
     chrome.storage.sync.get(["enabled"], function(items){
         if (items[0]) {
+            console.log("on");
             switchButton = 'on';
             $("#extension_state_switch").prop("checked", true);
         } else {
+            console.log("off");
             switchButton = "off";
             $("#extension_state_switch").prop("checked", false);
         }
@@ -72,22 +75,22 @@ $(function () {
 
 
     // handles the clicking of the switch and changes the state of the extension by sending message to background.js
-    // TODO catch these messages in background.js and do the work accordingly
     $("#extension_state_switch").click(function(){
         if(switchButton === 'off'){
             switchButton = 'on';
             console.log("switched to on");
+            //Set the state of the extension to enabled
             chrome.storage.sync.set({ "enabled": true }, () => {
                 console.log("Data Added");
             });
         }else{
             switchButton = 'off';
             console.log("switched to off");
+            //Set the state of the extension to disabled
             chrome.storage.sync.set({ "enabled": false }, () => {
                 console.log("Data Added");
             });
         }
     });
-    */
 
 });
